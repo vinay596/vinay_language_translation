d64e1d5555de62464ae56b87e4cd88e195346137
vinay596 <bollavaramvinaykumar@gmail.com>
2024-01-06 10:36:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import googletrans
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return render_template('translate.html')
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    text = request.json['text']
+    target_language = request.json['target_language']
+
+    translator = googletrans.Translator()
+    translated_text = translator.translate(text, dest=target_language).text
+
+    return jsonify({'translated_text': translated_text})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
